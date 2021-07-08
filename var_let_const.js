4f68ca6331c46b02facb641322664f4b4f688825
pamuba <partha.bora24@gmail.com>
2021-07-08 22:56:19 +0530

    12

--- var_let_const.js
+++ var_let_const.js
@@ -1,1 +1,17 @@
-var a;
+//1.Scoping  let is block scoped  var is global
+function fun(){
+    var a = "LOL"
+    let b = "BUZZ"
+
+    console.log(a,' ',b)
+
+    {
+        c = "horse"
+        let d = "apple"
+    }
+    console.log(c)
+    // console.log(d)
+}
+
+fun()
+console.log("-------------------"+c)
