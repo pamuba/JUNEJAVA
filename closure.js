0c95287a0c4d25911a2d18f30ae4c0f1ba5a6a83
pamuba <partha.bora24@gmail.com>
2021-07-08 23:42:27 +0530

    19

--- closure.js
+++ closure.js
@@ -1,1 +1,22 @@
-//capturing outer variables variables + argumenst
+//capturing outer variables variables + arguments
+
+function outer(a,b){
+    var outerA = 22;
+
+    function inner(c){
+        console.log(outerA);
+        console.log(a);
+        console.log(b);
+
+        function innermost(){
+            console.log("From Innermost:"+outerA)
+        }
+        innermost()
+    }
+    return inner;
+}
+
+var inn = outer(77,99);
+setTimeout(
+    ()=>{inn(33)}, 3000
+)
