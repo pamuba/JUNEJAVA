e995490c5f7c4d38fc0c0545f56bed9bf395b39b
pamuba <partha.bora24@gmail.com>
2021-07-08 23:09:53 +0530

    13

--- var_let_const.js
+++ var_let_const.js
@@ -1,17 +1,49 @@
 //1.Scoping  let is block scoped  var is global
-function fun(){
-    var a = "LOL"
-    let b = "BUZZ"
+// function fun(){
+//     var a = "LOL"
+//     let b = "BUZZ"
 
-    console.log(a,' ',b)
+//     console.log(a,' ',b)
 
-    {
-        c = "horse"
-        let d = "apple"
-    }
-    console.log(c)
-    // console.log(d)
-}
+//     {
+//         c = "horse"
+//         let d = "apple"
+//     }
+//     console.log(c)
+//     // console.log(d)
+// }
 
-fun()
-console.log("-------------------"+c)
+// fun()
+// console.log("-------------------"+c)
+
+
+// var funcs = [];
+// //3 fncs and store in the array
+// for (let i = 0; i < 3; i++) {
+//     funcs[i] = function(){
+//         console.log("My Func:"+i)
+//     }
+// }
+
+// for (let j = 0; j < 3; j++) {
+//     funcs[j]();
+// }
+
+//2. HOISTING TO THE TOP
+// function fun(){
+//     // var a;
+//     //let a;
+
+//     console.log(a)
+//     let a = 101
+//     // console.log(a)
+// }
+
+
+//3. redecleration
+// let a = 111;
+// let a = 222;
+// console.log(a)
+
+
+
