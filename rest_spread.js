1f3348a51978a834b1cd7089e510b4542ee3b90e
pamuba <partha.bora24@gmail.com>
2021-07-08 23:20:24 +0530

    134

---
+++ rest_spread.js
@@ -0,0 +1,20 @@
+//rest of the params
+//argu vs params
+
+// function add(i,j=10){
+//     console.log("SUM:",(i+j))
+// }
+
+// add(1)
+
+// function add(a,b,...args){
+//     let sum = 0;
+//     for(let z of args){
+//         sum+=z;
+//     }
+//     console.log("SUM:",sum)
+//     console.log(args instanceof Array)
+//     console.log(`A:${a} B:${b}`)
+// }
+
+// add(1,2,3,4,5)
