6be29a57cfebb7467748ac2ab84f7a9085ac99ed
pamuba <partha.bora24@gmail.com>
2021-07-12 23:55:55 +0530

    31

---
+++ generators.js
@@ -0,0 +1,31 @@
+// function* generator(){
+//     yield 1;
+//     yield 2;
+//     yield 3;
+//     yield 4;
+// }
+
+// let gen = generator();
+
+// console.log(gen.next().value)
+// console.log(gen.next().value)
+// setTimeout(
+//     ()=>{
+//         console.log(gen.next().value)
+//         console.log(gen.next().value)
+//     }, 2000
+// )
+
+
+// function* generator(){
+//     let index = 0
+
+//     while(true){
+//         yield index++
+//     }
+// }
+
+// let gen = generator();
+
+// console.log(gen.next().value)
+// console.log(gen.next().value)
