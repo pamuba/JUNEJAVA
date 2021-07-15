12dbe184056c26996b07ce4076d2dc7e17cdc56a
pamuba <partha.bora24@gmail.com>
2021-07-15 23:17:18 +0530

    41

---
+++ controller.js
@@ -0,0 +1,11 @@
+var myApp = angular
+                .module('myModule', [])
+                .controller('myController', function($scope){
+                    var employee = {
+                        firstName:"John",
+                        lastName:"Smith",
+                        gender:"Male",
+                        logo:'bore.jpg'
+                    };
+                    $scope.employee = employee;
+                });
