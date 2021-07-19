9018be87a5bb6884582065510c022999c2e3bf8f
pamuba <partha.bora24@gmail.com>
2021-07-20 00:38:32 +0530

    77

--- MEANJS/public/controller/controller.js
+++ MEANJS/public/controller/controller.js
@@ -1,7 +1,18 @@
 function AppCtrl($scope, $http) {
     console.log('hello world from Controller')
 
-    $http.get('/contactlist').success(function(response){
-        $scope.contactlist = response
-    })
+    var refresh = function(){
+        $http.get('/contactlist').success(function(response){
+            $scope.contactlist = response
+            $scope.contact = ''
+        })
+    }
+    refresh();
+    $scope.addContact = function(){
+        console.log($scope.contact);
+        $http.post('/contactlist',$scope.contact).success(function(response){
+            console.log(response);
+            refresh();
+        })
+    }
 }
--- MEANJS/server.js
+++ MEANJS/server.js
@@ -1,35 +1,30 @@
 var express = require('express')
+var mongojs = require('mongojs')
+var db = mongojs('contactlist',['contactlist'])
 
 var app = express()
 
+//middlewares
 app.use(express.static(__dirname+'/public'))
+app.use(express.json())
 
 app.get('/contactlist', function(req, res){
 
-    console.log("Server  received the request");
-
-    var person1 = {
-        name:"John",
-        email:"john@gmail.com",
-        number:'(111) 111-1111'
-    }
-    var person2 = {
-        name:"JIll",
-        email:"jill@gmail.com",
-        number:'(222) 222-2222'
-    }
-    var person3 = {
-        name:"James",
-        email:"james@gmail.com",
-        number:'(333) 333-3333'
-    }
-    var person4 = {
-        name:"Sally",
-        email:"sally@gmail.com",
-        number:'(444) 444-4444'
-    }
-    var contactlist = [person1, person2, person3, person4]
-    res.json(contactlist)
+    db.contactlist.find(function(err, docs){
+        if(!err){
+            console.log(docs)
+            res.json(docs)
+        }
+        else
+           console.log("Error retrieving data from database")
+    })
+    
+})
+app.post('/contactlist', function(req, res){
+    console.log(req.body)
+    db.contactlist.insert(req.body, function(err, doc){
+        res.json(doc)
+    })
 })
 
 app.listen(3000)
